7adc684ee3f0028c7a0a9b6c422a0fbc071c637c
Stefan Seeland <stefan.seeland@rubicon.eu>
2022-02-13 17:27:31 +0100

    Minimal reproducable added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SkiaSharpMinimalTest
+
 Reproduction of some skiaSharpIssue
+
+Picture 32-33.bmp triggers some issue on reading from stream on calling "SKCodec.Create"`, while reading from file path and using system.drawing.bitmap is working.
+
+```
+ SKCodec.Create(stream, out var result); 
+```
---
+++ SkiaSharpMinimalTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using SkiaSharp;
+
+Console.WriteLine("Hello, World!");
+
+var tryToReadThese = Directory.GetFiles("./", "*.bmp");
+
+foreach (var file in tryToReadThese)
+{
+    // this works
+    var redingFromFilePathInsteadStream = SKCodec.Create(file);
+
+    var stream = File.OpenRead(file);
+
+#pragma warning disable CA1416 // Validate platform compatibility
+    // this also works
+    using var bmp = new System.Drawing.Bitmap(stream);
+#pragma warning restore CA1416 // Validate platform compatibility
+
+    stream.Position = 0;
+
+    try
+    {
+        // this fails on Picture 32-33.bmp for no known reason
+        var codec = SKCodec.Create(stream, out var result);
+        Console.WriteLine($"{file} - {codec.EncodedFormat}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"failed to read {file} using SkiaSharp while system.drawing and createing from file succeeds - {ex.Message}, codec used in file is {redingFromFilePathInsteadStream.EncodedFormat}");
+    }
+}
